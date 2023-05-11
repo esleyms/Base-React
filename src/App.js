1bc37115c3050072d96791c7490687141ca82e5b
esleyms <101306305+esleyms@users.noreply.github.com>
2023-05-10 22:48:00 -0300

    Teste 07

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import UserGreeting from './Components/UserGreeting';
+import ParentComponent from './Components/ParentComponent'
 
-function App() {
+
+
+
+
+class App extends Component {
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+     <ParentComponent/>
+     {/*<UserGreeting/>*/}
+
     </div>
   );
 }
-
+}
 export default App;
---
+++ src/Components/UserGreeting.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class UserGreeting extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome Vishwas</p>
+      </div>
+    )
+  }
+}
+
+export default UserGreeting
