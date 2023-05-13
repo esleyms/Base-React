46a77fccc637f00654121bbc4ab8d826c2e12bcc
esleyms <101306305+esleyms@users.noreply.github.com>
2023-05-13 00:45:35 -0300

    css atualizado

--- src/Components/Inline.js
+++ src/Components/Inline.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const heading = {
-  fontSize: '72x',
+  fontSize: '90x',
   color: 'blue'
 }
 
 function Inline() {
   return (
     <div>
+      <h1 className='error'>Error</h1>
       <h1 style={heading}>Inline</h1>
     </div>
   )
