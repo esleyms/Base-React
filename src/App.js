cb1d8d208c8b40923f184c54705bdf70ead3771e
esleyms <101306305+esleyms@users.noreply.github.com>
2023-05-13 20:58:46 -0300

    Form novo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg'
+import React, {Component} from 'react'
 import './App.css'
-import { Component } from 'react'
-import UserGreeting from './Components/UserGreeting'
-import ParentComponent from './Components/ParentComponent'
-import NameList from './Components/NameList'
-import Person from './Components/Person'
-import Stylessheet from './Components/Stylessheet'
-import Inline from './Components/Inline'
-import './appStyles.css'
-import styles from './appStyles.module.css'
+import Form from './Components/Form';
 class App extends Component {
   render() {
   return (
     <div className="App">
-      <h1 className='error'>Error</h1>
-      <h1 className={styles.success}>Success</h1>
-      <Inline/>
-      {/*<Stylessheet primary={true}/>*/ }
-      {/*<NameList/>*/}
-      {/*<UserGreeting/>*/}
-     {/*<ParentComponent/> Aula 15*/} 
-     
+    
+   <Form />
+   
 
     </div>
   );
