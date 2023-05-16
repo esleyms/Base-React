b3a0f661a6d02aec66c19d551872cb4487889f61
esleyms <101306305+esleyms@users.noreply.github.com>
2023-05-15 22:09:56 -0300

    ciclo novo

--- src/Components/LifecycleA.js
+++ src/Components/LifecycleA.js
@@ -3,34 +3,51 @@ import LifecycleB from './LifecycleB'
 
 
 class LifecycleA extends Component {
-constructor(props) {
-  super(props)
+  constructor(props) {
+   super(props)
 
-  this.state = {
-     name: "Vishwas"
-  }
+    this.state = {
+      name: "Vishwas"
+    }
     console.log('LifecycleA constructor')
-}
-    static getDerivedStateFromProps(props,state) {
-        console.log('LifegetDerivedStateFromProps')
-        return null
+  }
+
+  static getDerivedStateFromProps(props,state) {
+    console.log('LifegetDerivedStateFromProps')
+   return null
     }
 
     componentDidMount() {
-        console.log('ifegetDerivedStateFromProps')
+      console.log('ifegetDerivedStateFromProps')
     }
   render() {
     
-        console.log('LifecycleA render')
-    
-    return (
-        <div>
-     <div>Lifecycle A</div>
-     <LifecycleB/>
-     </div>
+   console.log('LifecycleA render')
+  }
+  shouldComponentUpdate() {
+    console.log('Lifecyclea ShouldComponentUpdate')
+    return true
+   
+  }
+        
+  getSnapshotBeforeUpdate(prevProps, preState) {
+    console.log('Lifecyclea getSnapshotBeforeUpdate')
+   
+    componentDidUpdate() {
+      console.log('Lifecyclea componentDidUpdate' )
+    }
+  
+}
+ render() {
+   console.log('LifecycleA render')
+    return(
+       <div>
+          <div>Lifecycle A</div>
+         <LifecycleB/>
+       </div>
     )
   }
-}
 
+}
 export default LifecycleA
 
