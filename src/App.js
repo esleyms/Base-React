890e7774b88b5c9e5ba79a14a42dc820ed8076fb
esleyms <101306305+esleyms@users.noreply.github.com>
2023-05-25 20:45:23 -0300

    Aula 37 - Render Props (Parte 2)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import ClickCounterTwo from './Components/ClickCounterTwo';
 import HoverCounterTwo from './Components/HoverCounterTwo';
+//import User from './Components/User';
+import Counter from './Components/Counter';
 
 
 
@@ -11,10 +13,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Counter>
+        {(count, incrementCount) => (
+          <ClickCounterTwo count={count} incrementCount={incrementCount}/>
+          )}
+        </Counter>
+        <Counter>
+        {(count, incrementCount) => (
+          <HoverCounterTwo count={count} incrementCount={incrementCount}/>
+          )}
+        </Counter>
    
-      <ClickCounterTwo/>
+      {/* <ClickCounterTwo/>
       <HoverCounterTwo/>
-      
+    <User render={(isLoggeIn) => isLoggeIn ?"Vishwas" : 'Guest'}/>*/}
+
       </div>
     )
   }
