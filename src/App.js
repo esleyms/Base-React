48f7e9e165130188caf22bdb8039ee2992fb503d
esleyms <101306305+esleyms@users.noreply.github.com>
2023-05-25 22:37:13 -0300

    Aula 39 - (Parte 2)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
-import ClickCounterTwo from './Components/ClickCounterTwo';
-import HoverCounterTwo from './Components/HoverCounterTwo';
-//import User from './Components/User';
-import Counter from './Components/Counter';
+import ComponentC from './Components/ComponentC';
+import { UserProvider } from './Components/UserContext';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Counter>
-        {(count, incrementCount) => (
-          <ClickCounterTwo count={count} incrementCount={incrementCount}/>
-          )}
-        </Counter>
-        <Counter>
-        {(count, incrementCount) => (
-          <HoverCounterTwo count={count} incrementCount={incrementCount}/>
-          )}
-        </Counter>
-   
-      {/* <ClickCounterTwo/>
-      <HoverCounterTwo/>
-    <User render={(isLoggeIn) => isLoggeIn ?"Vishwas" : 'Guest'}/>*/}
-
+        <UserProvider value="Vishwas">
+      <ComponentC/>
+      </UserProvider>
       </div>
     )
   }
