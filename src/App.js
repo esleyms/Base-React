013b75b4dfedd017501313b6aa23f4ec6796eb5b
esleyms <101306305+esleyms@users.noreply.github.com>
2023-06-01 17:37:03 -0300

    71 - 28 - useRef Hook Part 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React  from 'react';
 import './App.css';
 import Counter1 from './Components/Counter1';
+import Focuslnput from './Components/Focuslnput';
 
 
 function App() {
   return (
     
       <div className="App">
-        <Counter1/>
+        <Focuslnput/>
       </div>
     
   );
